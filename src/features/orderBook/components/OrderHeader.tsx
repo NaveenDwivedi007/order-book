aefc7404468dc79910f03de2b41b4edfcc9aa7f6
naveen <ndwivedi@deqode.com>
2023-06-19 16:31:22 +0530

    btn action created

--- src/features/orderBook/components/OrderHeader.tsx
+++ src/features/orderBook/components/OrderHeader.tsx
@@ -25,12 +25,20 @@ function OrderHeader({
       setPair(str)
     }
   }, [pairText])
+
+  function actionControlHandler(event:React.MouseEvent,cb:Function=()=>{}) {
+    event.stopPropagation()
+    cb()
+    
+  }
   return (
-    <div className='order-header'>
+    <div className='order-header' onClick={(e) => {
+      e.preventDefault()
+      e.stopPropagation()
+      toggleFn(e)
+    }}>
       <div className="section">
-        <button onClick={(e) => {
-          toggleFn(e)
-        }} className='action-btn'>
+        <button className='action-btn'>
           <FontAwesomeIcon className={isCollapsed ? `action-collapse-icon` : undefined} icon={faChevronDown} style={{ color: "#f2f2f2", }} />
         </button>
         <span className="order-type">
@@ -41,16 +49,24 @@ function OrderHeader({
         </span>
       </div>
       {!isCollapsed && <div className="section action">
-        <button className='action-btn'>
+        <button className='action-btn' onClick={(event)=>{
+          actionControlHandler(event)
+        }}>
           <FontAwesomeIcon icon={faBell} style={{ color: "#f2f2f2", }} />
         </button>
-        <button className='action-btn'>
+        <button className='action-btn' onClick={(event)=>{
+          actionControlHandler(event)
+        }}>
           <FontAwesomeIcon icon={faGear} style={{ color: "#f2f2f2", }} />
         </button>
-        <button className='action-btn'>
+        <button className='action-btn' onClick={(event)=>{
+          actionControlHandler(event)
+        }}>
           <FontAwesomeIcon icon={faMagnifyingGlassPlus} style={{ color: "#f2f2f2", }} />
         </button>
-        <button className='action-btn'>
+        <button className='action-btn' onClick={(event)=>{
+          actionControlHandler(event)
+        }}>
           <FontAwesomeIcon icon={faMagnifyingGlassMinus} style={{ color: "#f2f2f2", }} />
         </button>
       </div>}
